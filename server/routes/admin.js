9478d632913766bb1261f76a4e33030917f6e843
buraksahinerden <burak.erden@getir.com>
2023-09-27 10:06:53 +0300

    authorization added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,6 +6,25 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 
 const adminLayout = '../views/layouts/admin'
+const jwtSecret = process.env.JWT_SECRET
+
+
+const authMiddleware = (req,res,next) => {
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({ message : 'Unauthorized' })
+    }
+
+    try {
+        const decode = jwt.verify(token, jwtSecret);
+        req.userId = decode.userId;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message : 'Unauthorized' })
+        
+    }
+}
 
 router.get('/admin', async (req, res) => {
     try {
@@ -21,17 +40,36 @@ router.get('/admin', async (req, res) => {
 
 });
 
-
 router.post('/admin', async (req, res) => {
     try {
-        const { username, password } = req.body;
-        console.log(req.body)
-
-        res.render('admin/index', { locals, layout: adminLayout });
+      const { username, password } = req.body;
+      
+      const user = await User.findOne( { username } );
+  
+      if(!user) {
+        return res.status(401).json( { message: 'Invalid credentials' } );
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if(!isPasswordValid) {
+        return res.status(401).json( { message: 'Invalid credentials' } );
+      }
+  
+      const token = jwt.sign({ userId: user._id}, jwtSecret );
+      res.cookie('token', token, { httpOnly: true });
+      res.redirect('/dashboard');
+  
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
+  });
+
+router.get('/dashboard', authMiddleware ,async (req, res) => {
+    res.render('admin/dashboard')
 });
+
+
 
 
 router.post('/register', async (req, res) => {
@@ -42,21 +80,20 @@ router.post('/register', async (req, res) => {
         try {
             const user = await User.create({
                 username,
-                password:hashedPassword
+                password: hashedPassword
             });
             res.status(201).json({
-                message:'User Created', 
+                message: 'User Created',
                 user
             });
         } catch (error) {
-            if( error === 1100) res.status(409).json({message:'User already in use'});
-            res.status(500).json({message:'Internal server error'})
+            if (error === 1100) res.status(409).json({ message: 'User already in use' });
+            res.status(500).json({ message: 'Internal server error' })
         }
-       
-    } catch (error) { 
+
+    } catch (error) {
         console.log(error);
     }
 });
 
-
-module.exports = router; 
+module.exports = router;
